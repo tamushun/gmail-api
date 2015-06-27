f8aa71006ba954760446787f972324d9bdb329f3
tamushun <shuntamu@gmail.com>
2015-06-28 01:52:50 +0900

    Execute command 'mvn archetype:generate'

---
+++ google/src/main/java/jp/co/tamushun/google/App.java
@@ -0,0 +1,13 @@
+package jp.co.tamushun.google;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
